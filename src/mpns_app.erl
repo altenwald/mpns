c43111f0763488919b1351aebbb998f9bafe5eef
Manuel Rubio <manuel@altenwald.com>
2016-01-18 16:56:29 +0100

    merge app and sup modules

--- src/mpns_app.erl
+++ src/mpns_app.erl
@@ -1,9 +1,16 @@
 -module(mpns_app).
 
 -behaviour(application).
+-behaviour(supervisor).
 
 %% Application callbacks
 -export([start/2, stop/1]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, A), {I, {I, start_link, A}, transient, 5000, worker, [I]}).
 
 %% ===================================================================
 %% Application callbacks
@@ -16,13 +23,30 @@
         {ok, Pid::pid()} | {error, Reason::any()}.
 
 start(_StartType, _StartArgs) ->
-	Module = case application:get_env(mpns, module) of
-		{ok, M} -> M;
-		_ -> undefined
-	end,
-    mpns_sup:start_link(Module).
+    Module = case application:get_env(mpns, module) of
+        {ok, M} -> M;
+        _ -> undefined
+    end,
+    supervisor:start_link({local, ?MODULE}, ?MODULE, [Module]).
 
 -spec stop( State::any() ) -> ok.
 
 stop(_State) ->
     ok.
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+-spec init(Args::[]) ->
+    {ok,{{
+        RestartStrategy::supervisor:strategy(),
+        MaxR::non_neg_integer(),
+        MaxT::non_neg_integer()},
+        [ChildSpec::supervisor:child_spec()]}} | 
+    ignore.
+
+init([Module]) ->
+    {ok, {{one_for_one, 5, 10}, [
+        ?CHILD(mpns, [Module])
+    ]}}.
